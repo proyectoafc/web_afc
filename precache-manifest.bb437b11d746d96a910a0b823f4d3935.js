1a006b47750c37835e661e1ff7acc5647701a688
proyectoafc <supervisora.vanessa.afc@gmail.com>
2020-08-14 23:52:50 -0500

    Updates

---
+++ precache-manifest.bb437b11d746d96a910a0b823f4d3935.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c0dcf664fbaa2bc6362f78294ef847",
+    "url": "/web_afc/index.html"
+  },
+  {
+    "revision": "335a21cb7e4334715c3a",
+    "url": "/web_afc/static/css/main.126c14d6.chunk.css"
+  },
+  {
+    "revision": "6dae403249e78dfbe077",
+    "url": "/web_afc/static/js/2.0b75b7db.chunk.js"
+  },
+  {
+    "revision": "e1690def25f3a7d9aa00a43cf9fa9296",
+    "url": "/web_afc/static/js/2.0b75b7db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "335a21cb7e4334715c3a",
+    "url": "/web_afc/static/js/main.f5725dc1.chunk.js"
+  },
+  {
+    "revision": "da5ee87435efe8a22a44",
+    "url": "/web_afc/static/js/runtime-main.1052ec86.js"
+  },
+  {
+    "revision": "08e14f76f60849ca7adee3a696e92d28",
+    "url": "/web_afc/static/media/App.08e14f76.jpg"
+  },
+  {
+    "revision": "2a4a1286ad2afde23cd91e3a5c0c54de",
+    "url": "/web_afc/static/media/Esquema.2a4a1286.jpg"
+  },
+  {
+    "revision": "39dd3bfb58315652af9874cc0abf4311",
+    "url": "/web_afc/static/media/Mochila.39dd3bfb.jpg"
+  },
+  {
+    "revision": "555012ddc102df92463196adeb876241",
+    "url": "/web_afc/static/media/agentes_comunitarios.555012dd.jpg"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/web_afc/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/web_afc/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/web_afc/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/web_afc/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "199e9ec14308832c6a8a49bd96c1fde6",
+    "url": "/web_afc/static/media/bg4.199e9ec1.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/web_afc/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/web_afc/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/web_afc/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/web_afc/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/web_afc/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "64d6df93f4021466ce9d155ed049282d",
+    "url": "/web_afc/static/media/horacio.64d6df93.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/web_afc/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "57f77f86129cb2aace4ecf79ca279064",
+    "url": "/web_afc/static/media/logo_upch3.57f77f86.png"
+  },
+  {
+    "revision": "90cf7dee83e537f7cea8c13795644122",
+    "url": "/web_afc/static/media/magaly.90cf7dee.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/web_afc/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "fa2ddee3ff9ac49bac792c1da844e9a6",
+    "url": "/web_afc/static/media/moises.fa2ddee3.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/web_afc/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/web_afc/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/web_afc/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/web_afc/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/web_afc/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/web_afc/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/web_afc/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/web_afc/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/web_afc/static/media/studio-5.ef5c30ea.jpg"
+  }
+]);
